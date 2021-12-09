66b93b4d4afbde18ca68a7761728991ae44a2de4
Leithon English <leithonenglish@gmail.com>
2021-12-08 21:17:07 -0500

    made overlay more mobile friendly + simple ui updates + removed some unsed code

--- src/components/home/ProjectItem.tsx
+++ src/components/home/ProjectItem.tsx
@@ -1,5 +1,5 @@
+import React, { FC } from "react";
 import { Icon } from "@iconify/react";
-import React, { FC, HtmlHTMLAttributes, ReactHTMLElement } from "react";
 import { StructuredText } from "react-datocms";
 
 type ProjectItemProps = {
